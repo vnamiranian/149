f674a3c63b53ffa355b1750aaa30fafb6b8f6484
stpiwe <pingkan.wewengkang@gmail.com>
2013-11-26 20:55:59 -0800

    Skeleton Tracking added to main. Still in progress

--- Project32/Project32/main.cpp
+++ Project32/Project32/main.cpp
@@ -24,6 +24,8 @@ int main(){
 	openni::OpenNI::shutdown();
 	*/
 
+	for (int i = 0; i < 5; ++i)
+		printf("%d\n", i);
 
 
 	
@@ -37,18 +39,77 @@ int main(){
 		return 1;
 	}
 	
+	/*
+	TO DO here:
+	Initialize Serial Bluetooth Connection
+	*/
+
+	//Control to Abort program
+	int timeoutToAbort = 0;
+
 	niterc = userTracker.create();
-	
 	nite::UserTrackerFrameRef userTrackerFrame;
 	while (!wasKeyboardHit())
 	{
 		niterc = userTracker.readFrame(&userTrackerFrame);
 		//Do other per-frame processing stuff
-		printf("a");
+
+		//Detecting Person. note: We only need to detect one person
+		const nite::Array<nite::UserData>& users = userTrackerFrame.getUsers();
+		if (users.getSize() < 1){
+			printf("b");
+			/*
+			If no one is detected for more than 50 consecutive frames, abort program
+			*/
+			if (timeoutToAbort>50){
+				printf("No one is detected. Aborting program.\n");
+				break;
+			}
+			else {
+				timeoutToAbort++;
+				continue;
+			}
+		}	
+		const nite::UserData& user = users[0];
+		timeoutToAbort = 0;
+		printf("a\n"); 
+
+		/*
+		Initiate Skeleton Tracking of the User.
+		*/
+		//if (user.isNew())
+			userTracker.startSkeletonTracking(user.getId());
+		while (true){
+			bool tracked = false;
+			switch (user.getSkeleton().getState())
+			{
+				case nite::SKELETON_CALIBRATING:
+					printf("C\r");
+					break;
+				case nite::SKELETON_NONE:
+					printf("S\r");
+					break;
+				case nite::SKELETON_TRACKED:
+					printf("\nTracked!\n");
+					tracked = true;
+					break;
+				default:
+					printf("Unknown State\r");
+			}//switch
+			if (tracked) break;
+		}//while
+
+
+
+
+		
+
+
+		printf("reach\n");
 	}
 	
 
 
-
+	nite::NiTE::shutdown();
 	return 0;
 }
