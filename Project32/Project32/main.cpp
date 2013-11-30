d8356dc7b8a45d60adac88752f751474bdc0ef9c
vnamiranian <vestanamiranian@berkeley.edu>
2013-11-30 01:11:39 -0800

    Update main.cpp

--- Project32/Project32/main.cpp
+++ Project32/Project32/main.cpp
@@ -1,4 +1,3 @@
-
 #include "NiTE.h"
 #include <NiteSampleUtilities.h>
 using namespace nite;
@@ -113,3 +112,122 @@ int main(){
 	nite::NiTE::shutdown();
 	return 0;
 }
+
+
+/* NOT DONE YET
+Socket set up:
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <signal.h>
+
+
+/*#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <stddef.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+#include <pthread.h>
+
+
+int listener_d; //STORES THE MAIN LISTENER FOR THE SOCKET
+
+void error(char *msg){
+	fprintf(stderr, "%s: %s\n", msg, strerror(errno));
+	exit(1);
+}
+
+//CREATES AN INTERNET STREAMING SOCKET
+int open_listener_socket() {
+	int s = socket(PF_INET, SOCK_STREAM, 0);
+	if (s == -1) { error("Can't open socket"); }
+	return s;
+}
+
+
+void bind_to_port(int socket, int port) {
+	struct sockaddr_in name;
+	name.sin_family = PF_INET;
+	name.sin_port = (in_port_t)htons(30000);
+	name.sin_addr.s_addr = htonl(INADDR_ANY);
+	int reuse = 1;
+	if (setsockopt(socket, SOL_SOCKET, SO_REUSEADDR, (char *)&reuse, sizeof(int)) == -1) {
+		error("Can't set the reuse option on the socket");
+	}
+	int c = bind (socket, (struct sockaddr *) &name, sizeof(name));
+	if (c == -1) { error("Can't bind to socket");}
+}
+
+//SENDING A STRING MSG TO THE CLIENT
+int say(int socket, char *s) {
+	int result = send(socket, s, strlen(s), 0);
+	if (result == -1) {
+		fprintf(stderr, "%s: %s\n", "Error talking to the client", strerror(errno));
+	}
+	return result;
+}
+
+//FOR CTR+C THIS FUNCTION CLOSES THE SOCKET BEFORE THE PROGRAM ENDS
+void handle_shutdown(int sig) {
+	if (listener_d) { close(listener_d);}
+	fprintf(stderr, "Bye!\n");
+	exit(0);
+}
+
+
+
+
+
+
+
+int main(int argc, char *argv[])
+{
+	char *advice[] = {
+			"Take smaller bites\r\n",
+			"Go for the tight jeans. No they do NOT make you look fat.\r\n",
+			"One word: inappropriate\r\n",
+			"Just for today, be honest. Tell your boss what you *really* think\r\n",
+			"You might want to rethink that haircut\r\n"};
+	int reuse = 1;
+	int listener_d = socket(PF_INET, SOCK_STREAM, 0);
+	struct sockaddr_in name;
+	name.sin_family = PF_INET;
+	name.sin_port = (in_port_t)htons(30000);
+	name.sin_addr.s_addr = htonl(INADDR_ANY);
+
+	if (setsockopt(listener_d, SOL_SOCKET, SO_REUSEADDR, (char *)&reuse, sizeof(int)) == -1){
+		error("Can't set the reuse option on the socket");
+	}
+	//bind (listener_d, (struct sockaddr *) &name, sizeof(name));
+	if (bind (listener_d, (struct sockaddr *) &name, sizeof(name)) == -1){
+		error("Can't bind the port");
+	}
+	listen (listener_d, 10);
+
+
+	puts("Waiting for connection");
+	while (1) {
+		struct sockaddr_storage client_addr;
+		unsigned int address_size = sizeof(client_addr);
+		int connect_d = accept (listener_d, (struct sockaddr *)&client_addr, &address_size);
+		char *msg = advice[rand() % 5];
+		send (connect_d, msg, strlen(msg), 0);
+		close(connect_d);
+	}
+	return 0;
+}
+
+
+
+
+
+
+
+*/
